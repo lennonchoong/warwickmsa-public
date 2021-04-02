9851962b0b67b7a6b97592ecc1906cb78017a31a
Lennon Choong <lennonchoong@Lennons-MacBook-Air.local>
2021-04-02 02:27:33 +0100

    test

--- express.js
+++ express.js
@@ -3,12 +3,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-// app.use('/', express.static('src'));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.get("/", function (req, res) {
-    var responseText = "Hello World!<br>";
-    responseText += "<small>Requested at: " + req.requestTime + "</small>";
-    res.send(responseText);
-});
+// app.get("/", function (req, res) {
+//     var responseText = "Hello World!<br>";
+//     responseText += "<small>Requested at: " + req.requestTime + "</small>";
+//     res.send(responseText);
+// });
 
 app.listen(3000);
